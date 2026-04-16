cd761d0468910c83d510013e67c970ec42310b81
gcamarenaprog <gcamarenaprog@outlook.com>
2026-04-16 13:02:35 -0600

    - Minor bugs fixed

--- single.php
+++ single.php
@@ -1,33 +1,33 @@
 <?php
-  /**
-   * Template Name:      Biblioteca Enigmas
-   * Theme URI:          https://github.com/gcamarenaprog/bibliotecaenigmas
-   * Description Theme:  Sahifa theme personalized for bibliotecaenigmas.com website!
-   * Author:             Guillermo Camarena
-   * Author URL:         http://gcamarenaprog.com
-   * Path:               /
-   * File name:          single.php
-   * Description:        This file show a single post of blog.
-   * Date:               24-11-2025
-   */
+/**
+ * Template Name:      Biblioteca Enigmas
+ * Theme URI:          https://github.com/gcamarenaprog/bibliotecaenigmas
+ * Description Theme:  Sahifa theme personalized for bibliotecaenigmas.com website!
+ * Author:             Guillermo Camarena
+ * Author URL:         http://gcamarenaprog.com
+ * Path:               /
+ * File name:          single.php
+ * Description:        This file show a single post of blog.
+ * Date:               16-04-2026
+ */
 ?>
 
-<?php get_header (); ?>
-<?php tie_breadcrumbs (); ?>
+<?php get_header(); ?>
+<?php tie_breadcrumbs(); ?>
 
-<?php if (!have_posts ()) : ?>
+<?php if (!have_posts()) : ?>
   <div class="content">
-    <?php get_template_part ('framework/parts/not-found'); ?>
+    <?php get_template_part('framework/parts/not-found'); ?>
   </div>
 <?php endif; ?>
 
-<?php while (have_posts ()) :
-  the_post (); ?>
-  
-  <?php tie_setPostViews (); ?>
-  
+<?php while (have_posts()) :
+  the_post(); ?>
+
+  <?php tie_setPostViews(); ?>
+
   <?php
-    $get_meta = get_post_custom ($post->ID);
+  $get_meta = get_post_custom($post->ID);
   if (!empty($get_meta["tie_post_head_cover"][0]))
     $content_width = 660;
   if (!empty($get_meta["tie_sidebar_pos"][0]) && $get_meta["tie_sidebar_pos"][0] == 'full')
@@ -40,10 +40,10 @@
   <section>
 
     <!--/Meta-->
-    <?php get_template_part ('library/framework/blocks/blog/single/meta'); ?>
-    
+    <?php get_template_part('library/framework/blocks/blog/single/meta'); ?>
+
     <!--/Image-->
-    <?php get_template_part ('library/framework/blocks/blog/single/thumbnail'); ?>
+    <?php get_template_part('library/framework/blocks/blog/single/thumbnail'); ?>
 
   </section>
 
@@ -55,49 +55,50 @@
       <div class="entry">
 
         <!--/Content-->
-        <?php the_content (); ?>
+        <?php the_content(); ?>
 
-        <!--/agination-->
-        <?php wp_link_pages (array('before' => '<div class="page-link">' . __ti ('Pages:'), 'after' => '</div>')); ?>
+        <!--/Pagination-->
+        <?php wp_link_pages(array('before' => '<div class="page-link">' . __ti('Pages:'), 'after' => '</div>')); ?>
 
         <!--/Edit-->
-        <?php edit_post_link (__ti ('Edit'), '<span class="edit-link">', '</span>'); ?>
+        <?php edit_post_link(__ti('Edit'), '<span class="edit-link">', '</span>'); ?>
 
-      </div> <!--/Entry-->
+      </div>
 
       <!--/Share-->
-      <?php get_template_part ('library/framework/blocks/blog/single/share'); ?>
+      <?php get_template_part('library/framework/blocks/blog/single/share'); ?>
 
       <div class="clear"></div>
 
     </div>
-    
+
     <?php
-      # End of Post action ----------
-      do_action ('tie_end_of_post');
+    # End of Post action ----------
+    do_action('tie_end_of_post');
     ?>
 
-  </article><!--/Article-->
+  </article>
 
   <!--/Author-->
-  <?php get_template_part ('library/framework/blocks/blog/single/author'); ?>
+  <?php get_template_part('library/framework/blocks/blog/single/author'); ?>
 
 
   <!--/Navigation-->
-  <?php if (tie_get_option ('post_nav')): ?>
-  <?php get_template_part ('library/framework/blocks/blog/single/navigation'); ?>
+  <?php if (tie_get_option('post_nav')): ?>
+  <?php get_template_part('library/framework/blocks/blog/single/navigation'); ?>
+
 <?php endif; ?>
 
   <!--/Related-->
-  <?php if (tie_get_option ('related_position') != 'in')
-  get_template_part ('library/framework/blocks/blog/single/related'); ?>
+  <?php if (tie_get_option('related_position') != 'in')
+  get_template_part('library/framework/blocks/blog/single/related'); ?>
 
   <!--/Check also-->
-  <?php get_template_part ('library/framework/blocks/blog/single/check-also'); ?>
+  <?php get_template_part('library/framework/blocks/blog/single/check-also'); ?>
 
 <?php endwhile; ?>
 
   </div>
 
-<?php get_sidebar (); ?>
-<?php get_footer (); ?>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
